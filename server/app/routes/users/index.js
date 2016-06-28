4bdcf57295aeeaf87e418ef7d389fe9db8130628
antkim003@gmail.com <antkim003@gmail.com>
2016-06-28 17:22:05 -0400

    users routes w/ tests

--- server/app/routes/users/index.js
+++ server/app/routes/users/index.js
@@ -22,13 +22,15 @@ router.get('/:userId', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-
-
+    User.create(req.body)
+        .then(function(user) {
+            res.json(user);
+        }, next);
 });
 
 router.put('/:userId', function(req, res, next) {
     let userId = req.params.userId;
-    console.log('here is reqbody ', req.body);
+    console.log('here is reqbody ', req.body, 'id: ', userId);
     var _user;
     User.findById(userId).then(function(user) {
         let keys = Object.keys(req.body);
@@ -45,5 +47,10 @@ router.put('/:userId', function(req, res, next) {
 });
 
 router.delete('/:userid', function(req, res, next) {
-
+    User.findById(req.params.userid)
+        .then(function(user) {
+            user.remove();
+            console.log('removed', user);
+            res.json('destroyed');
+        }, next);
 });
