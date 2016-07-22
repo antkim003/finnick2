811d38df4e20ad799702fe7ae62c3eb67373e0fa
antkim003@gmail.com <antkim003@gmail.com>
2016-07-22 11:52:39 -0400

    updating landing page copy

--- src/components/landing.js
+++ src/components/landing.js
@@ -20,9 +20,9 @@ class Landing extends Component {
     return (
       <div className="home">
           <div className="container">
-            <h3>THIS IS THE LANDING PAGE</h3>
+            <h1>FINNICK 2.0 - Black Friday Data Aggregator</h1>
             <div>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Finnick is a tool used for the Black Friday Project. All parties will be able to login and input data in real time. 
             </div>
           </div>
 
