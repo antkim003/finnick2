c421d2acfe2acd10ba480204c573a73242a2f01b
antkim003@gmail.com <antkim003@gmail.com>
2016-07-22 11:46:34 -0400

    redeploy

--- server/env/development.js
+++ server/env/development.js
@@ -1,5 +1,6 @@
 module.exports = {
-  "DATABASE_URI": "mongodb://finnick:f!nn!ck@104.196.32.26:27017/bf-finnick-2",
+  // "DATABASE_URI": "mongodb://finnick:f!nn!ck@104.196.32.26:27017/bf-finnick-2",
+  "DATABASE_URI" : "mongodb://localhost:27017/finnick2",
   "SESSION_SECRET": "Optimus Prime is my real dad",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
