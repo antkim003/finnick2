3313c0f07381a4c5bc397ee09e9e614c629adbc3
Liz <elizabeth.chen@macys.com>
2016-07-26 18:38:04 -0400

    tile

--- src/components/combobulator.js
+++ src/components/combobulator.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import tilestyle from '../../browser/css/tile.css';
+
 
 class Combobulator extends Component {
   render () {
-    // console.log(this.props.session, this.props.authenticated);
+    console.log(this.props.session, this.props.authenticated);
+
     return (
       <div className="home">
           <div className="container">
             <h1>Combobulator</h1>
+            <div className='tile'></div>
           </div>
-
       </div>
+
     )
 
   }
