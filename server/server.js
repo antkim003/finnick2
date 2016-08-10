3188bb13ff584a29d333e0b9ec88bbb2f796b887
antkim003@gmail.com <antkim003@gmail.com>
2016-08-09 22:25:59 -0400

    reverst

--- server/server.js
+++ server/server.js
@@ -7,24 +7,15 @@ const isDeveloping = process.env.NODE_ENV !== 'production';
 const port = isDeveloping ? 3000 : process.env.PORT;
 const startDb = require('./db');
 const app = require('./app');
-const sticky = require('socketio-sticky-session');
-const options = {
-  proxy: true,
-  header: 'x-forwarded-for',
-  num: 2,
-  sync: {
-    isSynced: true,
-    event: 'mySyncEventCall'
-  }
-}
+const server = require('http').createServer();
 
-sticky(options, function() {
-  const server = require('http').createServer(function(req, res) {
-    console.info('==> Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
-  });
-  server.on('request', app);
-  require('./io')(server);
-  return server;
-}).listen(port, function() {
-  console.log(`server started on ${port} port`);
+server.on('request', app);
+require('./io')(server);
+
+server.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
+
 });
