7f20315fc449fe585a79b0a58b89ac6013c4a43b
Liz <elizabeth.chen@macys.com>
2016-08-18 00:37:31 -0400

    preview text

--- browser/preview-tile-ind-cell.js
+++ browser/preview-tile-ind-cell.js
@@ -30,6 +30,12 @@ render() {
     var query = window.location.search.split('?')[1];
     var filteredname = _.filter(data.entries, function(d) { return d.hasOwnProperty('name') });
     var filteredimage = _.filter(data.entries, function(d) { return d.hasOwnProperty('imageid') });
+    var filteredcopy = _.filter(data.entries, function(d) { return d.hasOwnProperty('tilecopy1') });
+    var filteredcopy2 = _.filter(data.entries, function(d) { return d.hasOwnProperty('tilecopy2') });
+    var filteredcopy3 = _.filter(data.entries, function(d) { return d.hasOwnProperty('tilecopy3') });
+    var filteredcopy4 = _.filter(data.entries, function(d) { return d.hasOwnProperty('tilecopy4') });
+
+
     var img = '';
     if (filteredimage) {
         img = '<img src="http://slimages.macys.com/is/image/MCY/products/6/optimized/'+filteredimage[0].imageid+'_fpx.tif?bgc=255,255,255&amp;wid=228&amp;qlt=90,0&amp;fmt=jpeg" className="tile-img"/>'
@@ -38,7 +44,6 @@ render() {
     }
 //    <img src="http://slimages.macys.com/is/image/MCY/products/6/optimized/3149862_fpx.tif?bgc=255,255,255&amp;wid=228&amp;qlt=90,0&amp;fmt=jpeg" className="tile-img"/>
     function createMarkup() { return {__html: img}; };
-
     return (
         <div {...props} className={'tile'}>
             <div className="tile-img-wrap">
@@ -48,11 +53,11 @@ render() {
             </div>
             <div className="tile-content">
                 <span className="tile-title_1 tile-title">{filteredname[0].name}</span>
-                <span className="tile-title_2 tile-title">{filteredname[0].tilecopy1}</span>
+                <span className="tile-title_2 tile-title">{filteredcopy[0].tilecopy1}</span>
                 <p className="tile-desc">
-                    <span className="tile-desc-line_1 tile-desc-line">{filteredname[0].tilecopy2}</span>
-                    <span className="tile-desc-line_2 tile-desc-line">{filteredname[0].tilecopy3}</span>
-                    <span className="tile-desc-line tile-desc-line_3">{filteredname[0].tilecopy4}</span>
+                    <span className="tile-desc-line_1 tile-desc-line">{filteredcopy2[0].tilecopy2}</span>
+                    <span className="tile-desc-line_2 tile-desc-line">{filteredcopy3[0].tilecopy3}</span>
+                    <span className="tile-desc-line tile-desc-line_3">{filteredcopy4[0].tilecopy4}</span>
                 </p>
                 <div className="tile-btns">
                     <a href="/#/some-link-here-to-add-to-your-list" className="tile-btn_add tile-btn tile-btn_addToList">ADD TO LIST</a>
