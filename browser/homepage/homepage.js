e5e24010b02d9aa78d64502b531f56b46ac9adfe
Liz <elizabeth.chen@macys.com>
2016-09-21 14:51:24 -0400

    upload to gcloud

--- browser/homepage/homepage.js
+++ browser/homepage/homepage.js
@@ -1,12 +1,9 @@
 var React = require('react');
 import Loader from '../../src/components/loader/loader.js';
-//var Script = require('../../browser/homepage/jQuery.js');
 
-//var Script3 = require('../../browser/homepage/isotope.pkgd.min.js');
-//var Script4 = require('../../browser/homepage/imagesloaded.pkgd.min.js');
-//var Script5 = require('../../browser/homepage/fit-columns.js');
-//var Script1 = require('../../browser/homepage/scripts.js');
-//var Script2 = require('../../browser/homepage/scripts2.js');
+
+
+
 
 var HomepageComponent = React.createClass({
     displayName: 'HomepageComponent',
@@ -20,8 +17,39 @@ var HomepageComponent = React.createClass({
     componentDidMount() {
         var self = this;
 
-        var content = 'test sdfsdfsfs sdf';
+    var mapnames = {
+        'intlWomen' : "International Women",
+        'IntlWomen' : "International Women",
+        'intljuniors': "International Juniors",
+        'intlforthehome': 'International For the Home',
+        'intlkitchen&dining': 'International Kitchen and Dining',
+        'intlbed&bath': 'International Bed and Bath',
+        'intlluggage&accessories': 'International Luggage and Accessories',
+        'handbags&accessories': 'Handbags and Accessories',
+        'juniors': 'Juniors',
+        'beauty': 'Beauty',
+        'for_the_home': 'For the Home',
+        'kitchen&dining': 'Kitchen and Dining',
+        'bed&bath': 'Bed and Bath',
+        'luggage&accessories': 'Luggage and Accessories',
+        'furniture&mattresses': "Furniture and Mattresses",
+        'intlmen': 'International Men',
+        'intlkids': 'International Kids',
+        'intlshoes': "International Shoes",
+        "intljewelry&Watches": 'International Jewelry and Watches',
+        "intlhandbags&Accessories": 'International Handbags and Accessories',
+        'women': "Women",
+        "men": "Men",
+        "kids": "Kids",
+        "shoes": 'Shoes',
+        'jewelry&watches': 'Jewelry and Watches',
+        'jewlery&watches': 'Jewelry and Watches'
+    }
 
+
+
+    var content = 'test sdfsdfsfs sdf';
+    var d = new Date;
 
         var template = '\
         <div id="header">\
@@ -42,29 +70,22 @@ var HomepageComponent = React.createClass({
         <div id="home-page">\
             <div id="tile-container">\
         <% _.each(rows, function(row, idx) { %> \
-            <div class="tile home-tile" fob="<%=row.fob%>" tall="true">\
-                <%=row.fob%>\
-                <h6>Women</h6>\
+            <div class="tile home-tile" fob="<%=mapnames[row.entries[3]["doubleexposure"]]%>" tall="true">\
+                <h6><%=mapnames[row.entries[3]["doubleexposure"]]%></h6>\
                 <div class="image-wrap">\
-                    <img class="product-image mobile-image" src="images/mobile-women.jpg"/>\
-                    <img class="product-image desktop-image" src="images/desktop-women.jpg"/>\
-                    <span class="share-pinterest"><img src="images/mobile-icon-pinterest.png"/></span>\
+                    <img class="product-image mobile-image" src="http://storage.googleapis.com/imp-projects/finnick/homepagepreview/uploads/<%=row.entries[3]["doubleexposure"]%>-mobile.jpg?<%=d%>"/>\
+                    <img class="product-image desktop-image" src="http://storage.googleapis.com/imp-projects/finnick/homepagepreview/uploads/<%=row.entries[3]["doubleexposure"]%>.jpg?<%=d%>"/>\
+                    <span class="share-pinterest"><img src="http://storage.googleapis.com/imp-projects/finnick/homepagepreview/images/mobile-icon-pinterest.png"/></span>\
                 </div>\
                 <div class="copy-wrap">\
-                    <p class="line-1">Doorbuster</p>\
-                    <p class="line-2">60%&nbsp;off</p>\
-                    <p class="line-3">Select&nbsp;Women\'s&nbsp;Coats</p>\
-                    <p class="price">Reg.&nbsp;$160-$400</p>\
+                    <p class="line-1"><%=row.entries[30]["tilecopy1"]%></p>\
+                    <p class="line-2"><%=row.entries[31]["tilecopy2"]%></p>\
+                    <p class="line-3"><%=row.entries[32]["tilecopy3"]%></p>\
+                    <p class="price"><%=row.entries[33]["tilecopy4"]%></p>\
                 </div>\
             </div>\
         <% }) %>\
             </div>\
-        </div>\
-        \
-        \
-        \
-        <div>\
-             <span><%= c %></span>\
         </div>\
         ';
 
@@ -85,12 +106,16 @@ var HomepageComponent = React.createClass({
                     return row.entries[3]['doubleexposure'] != ''
                 });
 
-
+                setTimeout(function() {
+                    $('iframeforhome').trigger('resize');
+                })
 
                 var templateCompile = _.template(template)({
                     c: content,
                     title: "Black&nbsp;Friday",
-                    rows: data[0]["homepage"]
+                    rows: filledoutrows,
+                    mapnames: mapnames,
+                    d: d.getTime()
                 });
 
                 var newcontent = $("<div></div>").html(templateCompile).html();
@@ -102,6 +127,7 @@ var HomepageComponent = React.createClass({
 
                 var iframe = document.createElement('iframe');
                 iframe.className = "iframeforhome";
+                iframe.id = 'iframeforhome';
                 document.getElementById('contain').appendChild(iframe);
                 iframe.src = 'javascript:void((function(){var script = document.createElement(\'script\');' +
                     'script.innerHTML = "(function() {' +
@@ -109,7 +135,8 @@ var HomepageComponent = React.createClass({
                     '\';document.close();})();";' +
                     'document.write("<head>" + script.outerHTML + "</head><body></body>");})())';
                 iframe.contentWindow.document.write(
-                        '<link rel="stylesheet" type="text/css" href="http://storage.googleapis.com/imp-projects/finnick/homepagepreview/styles.css"/>'+
+                        '<link rel="stylesheet" type="text/css" href="http://storage.googleapis.com/imp-projects/finnick/homepagepreview/styles.' +
+                        'css"/>'+
                         '<link rel="stylesheet" type="text/css" href="http://storage.googleapis.com/imp-projects/finnick/homepagepreview/styles2.css"/>'+
                         '<script src="http://storage.googleapis.com/imp-projects/finnick/homepagepreview/jQuery.js"></sc'+'ript>'+
                         '<script src="http://storage.googleapis.com/imp-projects/finnick/homepagepreview/imagesloaded.pkgd.min.js"></sc'+'ript>'+
@@ -117,8 +144,8 @@ var HomepageComponent = React.createClass({
                         '<script src="http://storage.googleapis.com/imp-projects/finnick/homepagepreview/fit-columns.js"></sc'+'ript>'+
                         '<script src="http://storage.googleapis.com/imp-projects/finnick/homepagepreview/scripts.js"></sc'+'ript>'+
                         '<script src="http://storage.googleapis.com/imp-projects/finnick/homepagepreview/scripts2.js"></sc'+'ript>'+
-
-
+                        '<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">'+
+                        '<script>setTimeout(function() {$(window).trigger("resize");}, 3000)</script>'+
                         '<div id="countdown">'+
                         'Starts Wednesday &mdash; only <span class="countdown-cell">00</span> days left!'+
                         '</div>'+
--- src/components/homepagepreview.js
+++ src/components/homepagepreview.js
@@ -10,11 +10,60 @@ class HomepagePreview extends Component {
   }
 
   render () {
+        var mapnames = {
+            'intlWomen' : "International Women",
+            'IntlWomen' : "International Women",
+            'intljuniors': "International Juniors",
+            'intlforthehome': 'International For the Home',
+            'intlkitchen&dining': 'International Kitchen and Dining',
+            'intlbed&bath': 'International Bed and Bath',
+            'intlluggage&accessories': 'International Luggage and Accessories',
+            'handbags&accessories': 'Handbags and Accessories',
+            'juniors': 'Juniors',
+            'beauty': 'Beauty',
+            'for_the_home': 'For the Home',
+            'kitchen&dining': 'Kitchen and Dining',
+            'bed&bath': 'Bed and Bath',
+            'luggage&accessories': 'Luggage and Accessories',
+            'furniture&mattresses': "Furniture and Mattresses",
+            'intlmen': 'International Men',
+            'intlkids': 'International Kids',
+            'intlshoes': "International Shoes",
+            "intljewelry&Watches": 'International Jewelry and Watches',
+            "intlhandbags&Accessories": 'International Handbags and Accessories',
+            'women': "Women",
+            "men": "Men",
+            "kids": "Kids",
+            "shoes": 'Shoes',
+            'jewelry&watches': 'Jewelry and Watches',
+            'jewlery&watches': 'Jewelry and Watches',
+        }
+
     return (
       <div className="home">
           <div className="container">
             <h1>Homepage Preview</h1>
-            <div>
+            <div className="formforupload">
+                upload image by fob, please select whether mobile image or not
+                <form action="/api/combobulator/upload" method="post" encType="multipart/form-data">
+                    <select name="fob">
+                        <option>select fob</option>
+                        <option value="women">Women</option>
+                        <option value="men">Men</option>
+                        <option value="kids">Kids</option>
+                        <option value="shoes">Shoes</option>
+                        <option value="jewlery&watches">Jewelry and Watches</option>
+                        <option value="beauty">Beauty</option>
+                    </select>
+                    is this for mobile
+                    <select name="mobile">
+                        <option value="false">no</option>
+                        <option value="true">yes</option>
+                    </select>
+
+                    <input type="file" name="displayImage"/>
+                    <input type="submit"/>
+                </form>
                 <HomepageComponent />
             </div>
           </div>
